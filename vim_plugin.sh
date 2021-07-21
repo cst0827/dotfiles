f38bfda10252e8bd7d767dd8212c8417c9c22d1e
Peter <pe3398530@gmail.com>
2021-07-21 18:04:19 +0800

    Add script to clone vim plugin
    TODO:
        Manage plugin according to VIM version

---
+++ vim_plugin.sh
@@ -0,0 +1,7 @@
+# vim7 with Pathogen
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+
+# vim8
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+git clone https://github.com/brookhong/cscope.vim.git ~/.vim/pack/default/start/Cscove
