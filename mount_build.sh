94266e4c7eeab788538088080b42a0960aea0894
__cst__ <pe3398530@gmail.com>
2022-10-11 17:38:48 +0800

    Add script for mounting BuildServer

---
+++ mount_build.sh
@@ -0,0 +1,7 @@
+EXEC_USER=$USER
+[[ -d /home/$EXEC_USER/Desktop/BuildServer ]] || mkdir /home/$EXEC_USER/Desktop/BuildServer
+[[ -d /home/$EXEC_USER/Desktop/BuildServer/Automation ]] || mkdir /home/$EXEC_USER/Desktop/BuildServer/Automation
+[[ -d /home/$EXEC_USER/Desktop/BuildServer/Release ]] || mkdir /home/$EXEC_USER/Desktop/BuildServer/Release
+
+sudo mount.cifs -o vers=1.0 //192.168.128.6/Automation /home/$EXEC_USER/Desktop/BuildServer/Automation
+sudo mount.cifs -o vers=1.0 //192.168.128.6/Release /home/$EXEC_USER/Desktop/BuildServer/Release
