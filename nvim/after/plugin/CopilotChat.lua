f172142829856c4e1dbf367b8835b7e94c44a090
cst0827 <12411382+cst0827@users.noreply.github.com>
2025-07-01 17:23:03 +0800

    Add custom prompt for CopilotChat /Commit

--- nvim/after/plugin/CopilotChat.lua
+++ nvim/after/plugin/CopilotChat.lua
@@ -6,6 +6,9 @@ require("CopilotChat").setup {
         Optimize = {
             prompt = 'Optimize the selected code to improve performance and readability. Explain your optimization strategy and the benefits of your changes. Do not put the explanation between the header and its code block, put it after the code block.',
         },
+        Commit = {
+            prompt = 'Write commit message for the change with commitizen convention. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block. Format the commit message body using bullet points.'
+        },
     },
     mappings = {
         reset = { 
