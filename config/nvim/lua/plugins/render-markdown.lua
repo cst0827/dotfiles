2f1877fe4244a9f9247aef2375a2975f12e2573a
cst0827 <12411382+cst0827@users.noreply.github.com>
2026-05-18 19:15:37 +0800

    Fix render-markdown config

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,13 +1,16 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   version = "*",
+  lazy = false,
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   keys = {
     { "<leader>md", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
   },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    enabled = false,
+  },
   ---ft = { "markdown", "codecompanion" },
   ft = {},
 }
